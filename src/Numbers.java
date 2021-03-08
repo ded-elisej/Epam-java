eea975a0c6e882c8c690938025d5a80f72c58866
Tatyana <roimze@mail.ru>
2021-03-09 00:48:31 +0300

    the first linear programming equation

---
+++ src/Numbers.java
@@ -0,0 +1,11 @@
+public class Numbers<z> {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 4;
+        int c = 3;
+        double z;
+        z = (((a - 3) * (b / 2)) + c);
+        System.out.print(z);
+
+    }
+}
